f10efc102db8770d7befc0d840b4cf2792e4039b
Kauri Kont-Kontson <kaurikk@gmail.com>
2015-06-13 18:01:06 +0300

    [#96939502] Initial implementation

---
+++ Tests/ResourceHandlerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Managlea\Component\Tests;
+
+class ResourceHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function test()
+    {
+
+    }
+}
---
+++ src/ResourceHandlerInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Managlea\Component;
+
+
+interface ResourceHandlerInterface
+{
+    /**
+     * @param int $resourceId
+     */
+    public function getSingle($resourceId);
+
+    /**
+     * @param array $filters
+     */
+    public function getCollection(array $filters = array());
+
+    /**
+     * @param array $data
+     */
+    public function postSingle(array $data);
+
+    /**
+     * @param int $resourceId
+     * @param array $data
+     */
+    public function putSingle($resourceId, array $data);
+
+    /**
+     * @param int $resourceId
+     */
+    public function deleteSingle($resourceId);
+}
